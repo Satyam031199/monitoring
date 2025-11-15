e61c0d8d11376c1957fe762becfe5887d66220ea
Satyam Chaturvedi <satyamchaturvedi71@gmail.com>
2025-11-15 11:32:51 +0530

    add custom metrics

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from 'express';
 import doSomeHeavyTask from './util.js';
 import client from 'prom-client';
+import responseTime from 'response-time';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 const collectDefaultMetrics = client.collectDefaultMetrics;
 collectDefaultMetrics({ register: client.register });
+
+const reqResponseTime = new client.Histogram({
+  name: 'http_request_response_time_seconds',
+  help: 'Histogram of HTTP request response time',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [1,50,100,200,400,500,800,1000,2000,5000]
+});
+
+app.use(responseTime((req, res, time) => {
+  reqResponseTime.labels({ method: req.method, route: req.url, status_code: res.statusCode }).observe(time);
+}));
 
 app.get('/', (req, res) => {
   res.status(200).json({ success: true, message: 'Server is up and running!' });
